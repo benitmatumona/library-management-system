1391aad172cb83a90dd7723074f6f32294b12571
musa_windows11 <dondolomosa@gmail.com>
2026-05-11 16:06:51 +0200

    Initial commit

--- practical/starter-code/src/main/java/library/Librarian.java
+++ practical/starter-code/src/main/java/library/Librarian.java
@@ -8,16 +8,33 @@ package library;
 public class Librarian {
 
     // TODO: Declare private fields: name (String), staffId (String)
+    private String name;
+    private String staffId;
 
 
     // TODO: Constructor — Librarian(String name, String staffId)
+    public Librarian(String name, String staffId) {
+        this.name = name;
+        this.staffId = staffId;
+    }
 
 
     // TODO: Getters — getName(), getStaffId()
+    String getName() {
+        return this.name;
+    }
+
+    String getStaffId() {
+        return this.staffId;
+    }
 
 
     // TODO: issueItem(LibraryItem item) : String
     //   1. Calls item.checkOut()
     //   2. Returns: "[name] issued: [item.getSummary()]"
+    String issueItem(LibraryItem item) {
+        item.checkout();
+        return "[" + getName() + "] issued: [" + item.getSummary() + "]";
+    }
 
 }
